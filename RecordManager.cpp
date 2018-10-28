a0275c4b33bff433126848a31c08240d997eb167
ankutalev <a.kutalev@g.nsu.ru>
2018-10-28 16:14:02 +0700

    Forgotten files-todos

---
+++ RecordManager.cpp
@@ -0,0 +1,9 @@
+#include "RecordManager.h"
+
+//RecordManager::RecordManager(int serverSocket) {
+//    FD_ZERO(&clientSockets);
+//    FD_SET(serverSocket,&clientSockets);
+//
+//    std::fill(clientDescryptors,clientDescryptors+MAX_FILES_DESCRIPTORS,0);
+//
+//}
